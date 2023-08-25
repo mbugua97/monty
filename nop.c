c5a319619b56fa8155bb85608f70dc3dffb908db
job mbugua <jobugua97@gmail.com>
2023-08-25 09:51:16 +0300

    all tasks solved

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- empty
+  *@head: stack head
+  *@counter: line_number
+  *Return: nothing
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - top stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: nothing
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
